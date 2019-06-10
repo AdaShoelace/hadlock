200e3d03e267906d647b2c3524599215c6e75d03
PierreLeidbring <pierre.leidbring@gmail.com>
2019-06-10 20:55:59 +0200

    event loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate x11;
+extern crate x11_dl;
 extern crate libc;
 extern crate simplelog;
 
@@ -9,6 +10,6 @@ use windowmanager::*;
 
 fn main() {
     let mut window_manager = WindowManager::new();
-    //window_manager.run();
+    window_manager.run();
     loop {}
 }
--- src/windowmanager.rs
+++ src/windowmanager.rs
@@ -1,9 +1,10 @@
 
 use std::ptr;
-use x11::xlib;
-use x11::xlib::{Display, Window};
-use x11::xlib::{XOpenDisplay, XDefaultScreenOfDisplay, XRootWindowOfScreen};
+use x11_dl::xlib;
+use x11_dl::xlib::{Display, Window};
 use libc::{c_char, c_uchar, c_int, c_uint, c_long, c_ulong};
+use std::ffi::{CString, CStr};
+use std::mem;
 use std::process::exit;
 
 unsafe extern "C" fn error_handler(_: *mut xlib::Display, _: *mut xlib::XErrorEvent) -> c_int {
@@ -11,47 +12,87 @@ unsafe extern "C" fn error_handler(_: *mut xlib::Display, _: *mut xlib::XErrorEv
 }
 
 unsafe extern "C" fn on_wm_detected(_: *mut xlib::Display, e: *mut xlib::XErrorEvent) -> c_int {
+    if (*e).error_code == xlib::BadAccess {
+        panic!("Other wm registered!")
+    }
     return 0;
 }
+
 
 pub struct WindowManager {
     display: *mut Display,
     root : Window,
-    wm_detected: bool
 }
 
 impl WindowManager {
+
+    /*
+     * Open a connection to X display
+     * Check for failure
+     * return WindowManager
+     */
     pub fn new () -> Self {
         unsafe {
+            let xlib: xlib::Xlib = xlib::Xlib::open().unwrap();
+            let display = (xlib.XOpenDisplay)(ptr::null_mut());
 
-            let display = XOpenDisplay(ptr::null_mut());
-            let screen = XDefaultScreenOfDisplay(display);
-            let root = XRootWindowOfScreen(screen);
+            if display == std::ptr::null_mut() {
+                // Log with simplelog in the future
+                let disp_name = CString::from_raw((xlib.XDisplayName)(display as *const c_char));
+                println!("Failed to open display: {}", disp_name.to_str().unwrap());
+            }
 
+            // let screen = (xlib.XDefaultScreenOfDisplay)(display);
+            // let root = (xlib.XRootWindowOfScreen)(screen);
+            let root = (xlib.XDefaultRootWindow)(display);
             Self {
                 display: display,
                 root: root,
-                wm_detected: false
             }
         }
     }
 
     pub fn run(&mut self) {
         unsafe {
-            println!("Display name: {}\nDisplay height: {}", xlib::XDisplayName(ptr::null()) as i8, xlib::XDisplayHeight(self.display, 0) as u8);
-            xlib::XSetErrorHandler(Some(on_wm_detected));
-            xlib::XSelectInput(
+            let xlib: xlib::Xlib = xlib::Xlib::open().unwrap();
+
+            let cstring = CString::from_raw((xlib.XDisplayName)(self.display as *const c_char));
+            println!("Display name: {}\nroot: {}", cstring.to_str().unwrap(), self.root);
+            
+            (xlib.XSetErrorHandler)(Some(on_wm_detected));
+
+            (xlib.XSelectInput)(
                 self.display,
+                //self.root + 1,
                 self.root,
                 xlib::SubstructureRedirectMask | xlib::SubstructureNotifyMask
             );
-            xlib::XSync(self.display, 0);
-            if self.wm_detected {
-                return;
+
+            (xlib.XSync)(self.display, 0);
+            (xlib.XSetErrorHandler)(Some(error_handler));
+
+            println!("Pre loop");
+            loop {
+            println!("entered loop");
+                let e: *mut xlib::XEvent = mem::uninitialized();
+                (xlib.XNextEvent)(self.display, e);
+
+                match (*e).get_type() {
+                    xlib::CreateNotify => Self::on_create_notify((*e).create_window),
+                    xlib::ConfigureRequest => Self::on_configure_request((*e).configure_request),
+                    _ => println!("Unknown event")
+                }
             }
-            xlib::XSetErrorHandler(Some(error_handler));
+
         }
     }
 
+    unsafe fn on_configure_request(event: xlib::XConfigureRequestEvent) {
+        println!("Time to configure!");
+    }
+
+    unsafe fn on_create_notify(event: xlib::XCreateWindowEvent) {
+        println!("on_create_notify: called");
+    }
 
 }
