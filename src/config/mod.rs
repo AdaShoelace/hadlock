99cacc0ba3235fd0df85483f5a7734be20621930
PierreLeidbring <pierre.leidbring@gmail.com>
2019-08-31 15:52:05 +0000

    add support for executing an array of bash commands from config file

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -5,10 +5,13 @@ pub mod config_data;
 use lazy_static::*;
 use loader::*;
 use config_data::*;
+use serde::{Deserialize, Serialize};
 
 lazy_static! {
     pub static ref CONFIG: ConfigData = {
         loader::load_config()
     };
 }
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct Command { pub program: String, pub args: Vec<String> }
 
