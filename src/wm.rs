e3b20de8b0e5013bbb3bdb35e84a5054209a52c6
PierreLeidbring <pierre.leidbring@gmail.com>
2020-01-02 14:31:07 +0100

    move to ws

--- src/wm.rs
+++ src/wm.rs
@@ -67,7 +67,7 @@ pub fn get_mon_by_ws(state: &State, ws: u32) -> Option<MonitorId> {
 }
 
 pub fn set_current_ws(state: &mut State, ws: u32) -> Option<()> {
-
+    
     let mon = match get_mon_by_ws(state, ws) {
         Some(mon) => {
             state.monitors.get_mut(&mon)?
@@ -102,9 +102,61 @@ pub fn set_current_ws(state: &mut State, ws: u32) -> Option<()> {
     } else {
         mon.add_ws(Workspace::new(ws));
     }
+
     state.current_monitor = mon.id;
+    if mon.workspaces.get(&mon.current_ws)?.clients.is_empty() {
+        mon.remove_ws(mon.current_ws);
+    }
     mon.current_ws = ws;
     state.lib.move_cursor(Position { x: mon.screen.x + (mon.screen.width / 2), y: mon.screen.y + (mon.screen.height / 2) });
     mon.handle_state.replace(HandleState::Focus);
+    Some(())
+}
+
+pub fn move_to_ws(state: &mut State, w: Window, ws: u32) -> Option<()> {
+
+    let ww = state.monitors.get_mut(&state.current_monitor)?.remove_window(w);
+
+    let mon = match get_mon_by_ws(state, ws) {
+        Some(mon) => {
+            state.monitors.get_mut(&mon)?
+        },
+        None => {
+            state
+                .monitors
+                .get_mut(&state.current_monitor)?
+        }
+    };
+
+    if ws == mon.current_ws {
+        let new_ww = WindowWrapper {
+            handle_state: HandleState::Map.into(),
+            ..ww
+        };
+        mon.add_window(w, new_ww);
+        state.lib.unmap_window(w);
+        return Some(())
+    }
+
+    if mon.contains_ws(ws) {
+        let mut old_ws = mon.remove_ws(ws)?;
+        let new_ww = WindowWrapper {
+            handle_state: HandleState::Map.into(),
+            ..ww
+        };
+        old_ws.add_window(w, new_ww);
+        mon.add_ws(old_ws);
+        state.lib.unmap_window(w);
+    } else {
+        let mut new_ws = Workspace::new(ws);
+        let new_ww = WindowWrapper {
+            handle_state: HandleState::Map.into(),
+            ..ww
+        };
+        new_ws.add_window(w, new_ww);
+        mon.add_ws(new_ws);
+        state.lib.unmap_window(w);
+    }
+
     Some(())
 }
