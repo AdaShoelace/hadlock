0ffe61de51a9ac4af81198f3341f5232110d1d8a
PierreLeidbring <pierre.leidbring@gmail.com>
2020-01-04 23:17:49 +0100

    disable 'move window' if monocle

--- src/reducers/motion_notify.rs
+++ src/reducers/motion_notify.rs
@@ -1,25 +1,19 @@
 use {
     crate::{
-        wm,
-        models::{
-            window_type::WindowType,
-            rect::*,
-            windowwrapper::*,
-            HandleState
-        },
-        xlibwrapper::action,
-        xlibwrapper::masks::*,
-        xlibwrapper::core::*,
-        xlibwrapper::util::*,
-        state::State,
-        xlibwrapper::xlibmodels::*,
         config::CONFIG,
+        models::{rect::*, window_type::WindowType, windowwrapper::*, HandleState, WindowState},
+        state::State,
+        wm,
+        xlibwrapper::action,
+        xlibwrapper::core::*,
+        xlibwrapper::masks::*,
+        xlibwrapper::util::*,
+        xlibwrapper::xlibmodels::*,
     },
-    std::rc::Rc,
     reducer::*,
     std::cell::RefCell,
+    std::rc::Rc,
 };
-
 
 impl Reducer<action::MotionNotify> for State {
     fn reduce(&mut self, action: action::MotionNotify) {
@@ -32,21 +26,39 @@ impl Reducer<action::MotionNotify> for State {
                 .replace(HandleState::Focus);
         }
 
-        let drag_pos = Position { x: action.x_root, y: action.y_root };
-        let (delta_x, delta_y) =  (drag_pos.x - self.drag_start_pos.0,
-            drag_pos.y - self.drag_start_pos.1);
-
-        let dest_pos = Position{ x: self.drag_start_frame_pos.0 + delta_x,
-        y: self.drag_start_frame_pos.1 + delta_y};
-
+        let drag_pos = Position {
+            x: action.x_root,
+            y: action.y_root,
+        };
+        let (delta_x, delta_y) = (
+            drag_pos.x - self.drag_start_pos.0,
+            drag_pos.y - self.drag_start_pos.1,
+        );
+        let dest_pos = Position {
+            x: self.drag_start_frame_pos.0 + delta_x,
+            y: self.drag_start_frame_pos.1 + delta_y,
+        };
 
         if (action.state & (Button1Mask | Mod4Mask)) == Button1Mask | Mod4Mask {
-            let new_pos = Position{x: dest_pos.x, y: dest_pos.y};
-            let (pos, _) = self.monitors.get_mut(&self.current_monitor).expect("MotionNotify - monitor - get_mut").move_window(action.win, new_pos.x, new_pos.y);
-            let w = self.monitors.get_mut(&self.current_monitor).expect("MotionNotify - monitor - get_mut").get_client_mut(action.win).expect("motion_notify some window");
-            w.set_position(pos);
-            w.handle_state = HandleState::Move.into();
+            let new_pos = Position {
+                x: dest_pos.x,
+                y: dest_pos.y,
+            };
+            let (pos, _) = self
+                .monitors
+                .get_mut(&self.current_monitor)
+                .expect("MotionNotify - monitor - get_mut")
+                .move_window(action.win, new_pos.x, new_pos.y);
+            let w = self
+                .monitors
+                .get_mut(&self.current_monitor)
+                .expect("MotionNotify - monitor - get_mut")
+                .get_client_mut(action.win)
+                .expect("motion_notify some window");
+            if w.current_state != WindowState::Monocle {
+                w.set_position(pos);
+                w.handle_state = HandleState::Move.into();
+            }
         }
     }
 }
-
