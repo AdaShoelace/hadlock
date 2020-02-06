b471c5130aa17886e3d181a2cdf3e108f8902f89
PierreLeidbring <pierre.leidbring@gmail.com>
2020-02-06 14:20:54 +0100

    refactor of key_press

--- src/reducers/key_press.rs
+++ src/reducers/key_press.rs
@@ -10,6 +10,7 @@ use {
         xlibwrapper::action,
         xlibwrapper::core::*,
         xlibwrapper::masks::*,
+        xlibwrapper::util::keysym_lookup::*,
         xlibwrapper::util::*,
         xlibwrapper::xlibmodels::*,
     },
@@ -23,6 +24,9 @@ impl Reducer<action::KeyPress> for State {
     fn reduce(&mut self, action: action::KeyPress) {
         let mod_not_shift = (action.state & (Mod4Mask | Shift)) == Mod4Mask;
         let mod_and_shift = (action.state & (Mod4Mask | Shift)) == Mod4Mask | Shift;
+
+        let sym = self.lib.keycode_to_key_sym(action.keycode as u8);
+        debug!("KeyCode to string: {:?}", into_hdl_keysym(&sym));
 
         let ws_keys: Vec<u8> = (1..=9)
             .map(|x| {
@@ -74,155 +78,149 @@ fn managed_client(
             .get(&state.current_monitor)?
             .get_client(state.focus_w)?
             .get_size();
-        if state.lib.str_to_keycode("Right")? == keycode {
-            let mon = state.monitors.get_mut(&state.current_monitor)?;
+        match into_hdl_keysym(&state.lib.keycode_to_key_sym(keycode)) {
+            HDLKeysym::XK_Right => {
+                let mon = state.monitors.get_mut(&state.current_monitor)?;
 
-            let (_dec_size, size) =
-                mon.resize_window(state.focus_w, old_size.width + 10, old_size.height);
-            let ww = mon.remove_window(state.focus_w)?;
-            let new_ww = WindowWrapper {
-                window_rect: Rect::new(ww.get_position(), size),
-                handle_state: HandleState::Resize.into(),
-                ..ww
-            };
-            mon.add_window(state.focus_w, new_ww);
-
-            return Some(());
-        }
-        if state.lib.str_to_keycode("Left")? == keycode {
-            let mon = state.monitors.get_mut(&state.current_monitor)?;
-
-            let (_dec_size, size) =
-                mon.resize_window(state.focus_w, old_size.width - 10, old_size.height);
-            let ww = mon.remove_window(state.focus_w)?;
-            let new_ww = WindowWrapper {
-                window_rect: Rect::new(ww.get_position(), size),
-                handle_state: HandleState::Resize.into(),
-                ..ww
-            };
-            mon.add_window(state.focus_w, new_ww);
-            return Some(());
-        }
-        if state.lib.str_to_keycode("Down")? == keycode {
-            let mon = state.monitors.get_mut(&state.current_monitor)?;
-
-            let (_dec_size, size) =
-                mon.resize_window(state.focus_w, old_size.width, old_size.height + 10);
-            let ww = mon.remove_window(state.focus_w)?;
-            let new_ww = WindowWrapper {
-                window_rect: Rect::new(ww.get_position(), size),
-                handle_state: HandleState::Resize.into(),
-                ..ww
-            };
-            mon.add_window(state.focus_w, new_ww);
-            return Some(());
-        }
-        if state.lib.str_to_keycode("Up")? == keycode {
-            let mon = state.monitors.get_mut(&state.current_monitor)?;
-            let (_dec_size, size) =
-                mon.resize_window(state.focus_w, old_size.width, old_size.height - 10);
-            let ww = mon.remove_window(state.focus_w)?;
-            let new_ww = WindowWrapper {
-                window_rect: Rect::new(ww.get_position(), size),
-                handle_state: HandleState::Resize.into(),
-                ..ww
-            };
-            mon.add_window(state.focus_w, new_ww);
-            return Some(());
-        }
-        if state.lib.str_to_keycode("q")? == keycode {
-            let ww = state
-                .monitors
-                .get_mut(&state.current_monitor)?
-                .get_client_mut(state.focus_w)?;
-
-            ww.handle_state.replace(HandleState::Destroy);
-            return Some(());
-        }
-        if state.lib.str_to_keycode("e")? == keycode {
-            state.lib.exit();
-            return Some(());
-        }
-        if state.lib.str_to_keycode("f")? == keycode {
-            let ww = state
-                .monitors
-                .get_mut(&state.current_monitor)?
-                .remove_window(state.focus_w)?;
-            let new_ww = wm::toggle_monocle(state, ww);
-            state
-                .monitors
-                .get_mut(&state.current_monitor)?
-                .add_window(state.focus_w, new_ww);
-        }
-
-        match ws_keys.contains(&keycode) {
-            true => {
-                let ws_num = keycode_to_ws(keycode);
-                wm::move_to_ws(state, state.focus_w, ws_num);
-                wm::set_current_ws(state, ws_num)?;
+                let (_dec_size, size) =
+                    mon.resize_window(state.focus_w, old_size.width + 10, old_size.height);
+                let ww = mon.remove_window(state.focus_w)?;
+                let new_ww = WindowWrapper {
+                    window_rect: Rect::new(ww.get_position(), size),
+                    handle_state: HandleState::Resize.into(),
+                    ..ww
+                };
+                mon.add_window(state.focus_w, new_ww);
             }
-            _ => {}
+
+            HDLKeysym::XK_Left => {
+                let mon = state.monitors.get_mut(&state.current_monitor)?;
+
+                let (_dec_size, size) =
+                    mon.resize_window(state.focus_w, old_size.width - 10, old_size.height);
+                let ww = mon.remove_window(state.focus_w)?;
+                let new_ww = WindowWrapper {
+                    window_rect: Rect::new(ww.get_position(), size),
+                    handle_state: HandleState::Resize.into(),
+                    ..ww
+                };
+                mon.add_window(state.focus_w, new_ww);
+            }
+
+            HDLKeysym::XK_Down => {
+                let mon = state.monitors.get_mut(&state.current_monitor)?;
+
+                let (_dec_size, size) =
+                    mon.resize_window(state.focus_w, old_size.width, old_size.height + 10);
+                let ww = mon.remove_window(state.focus_w)?;
+                let new_ww = WindowWrapper {
+                    window_rect: Rect::new(ww.get_position(), size),
+                    handle_state: HandleState::Resize.into(),
+                    ..ww
+                };
+                mon.add_window(state.focus_w, new_ww);
+            }
+
+            HDLKeysym::XK_Up => {
+                let mon = state.monitors.get_mut(&state.current_monitor)?;
+                let (_dec_size, size) =
+                    mon.resize_window(state.focus_w, old_size.width, old_size.height - 10);
+                let ww = mon.remove_window(state.focus_w)?;
+                let new_ww = WindowWrapper {
+                    window_rect: Rect::new(ww.get_position(), size),
+                    handle_state: HandleState::Resize.into(),
+                    ..ww
+                };
+                mon.add_window(state.focus_w, new_ww);
+            }
+
+            HDLKeysym::XK_q => {
+                let ww = state
+                    .monitors
+                    .get_mut(&state.current_monitor)?
+                    .get_client_mut(state.focus_w)?;
+
+                ww.handle_state.replace(HandleState::Destroy);
+            }
+
+            HDLKeysym::XK_e => {
+                state.lib.exit();
+            }
+
+            HDLKeysym::XK_f => {
+                let ww = state
+                    .monitors
+                    .get_mut(&state.current_monitor)?
+                    .remove_window(state.focus_w)?;
+                let new_ww = wm::toggle_monocle(state, ww);
+                state
+                    .monitors
+                    .get_mut(&state.current_monitor)?
+                    .add_window(state.focus_w, new_ww);
+            }
+            _ => {
+                if ws_keys.contains(&keycode) {
+                    let ws_num = keycode_to_ws(keycode);
+                    wm::move_to_ws(state, state.focus_w, ws_num);
+                    wm::set_current_ws(state, ws_num)?;
+                }
+            }
         }
+        return Some(());
     }
 
     if mod_not_shift {
         println!("Number pressed");
 
-        if state.lib.str_to_keycode("f")? == keycode {
-            let ww = state
-                .monitors
-                .get_mut(&state.current_monitor)?
-                .remove_window(state.focus_w)?;
-            let new_ww = wm::toggle_maximize(state, ww);
-            state
-                .monitors
-                .get_mut(&state.current_monitor)?
-                .add_window(state.focus_w, new_ww);
-            return Some(());
-        }
-        if state.lib.str_to_keycode("Right")? == keycode
-            || state.lib.str_to_keycode("l")? == keycode
-        {
-            shift_window(state, Direction::East);
-            return Some(());
-        }
-        if state.lib.str_to_keycode("Left")? == keycode || state.lib.str_to_keycode("h")? == keycode
-        {
-            shift_window(state, Direction::West);
-            return Some(());
-        }
-        if state.lib.str_to_keycode("Down")? == keycode || state.lib.str_to_keycode("j")? == keycode
-        {
-            shift_window(state, Direction::South);
-            return Some(());
-        }
-        if state.lib.str_to_keycode("Up")? == keycode || state.lib.str_to_keycode("k")? == keycode {
-            shift_window(state, Direction::North);
-            return Some(());
-        }
-        if state.lib.str_to_keycode("c")? == keycode {
-            let mon = state.monitors.get_mut(&state.current_monitor)?;
-            let ww = mon.remove_window(state.focus_w)?;
-            let (pos, size) = mon.place_window(ww.window());
-            let new_ww = WindowWrapper {
-                window_rect: Rect::new(size, pos),
-                previous_state: ww.current_state,
-                current_state: WindowState::Free,
-                handle_state: HandleState::Center.into(),
-                ..ww
-            };
-            mon.add_window(state.focus_w, new_ww);
-            return Some(());
-        }
-        if state.lib.str_to_keycode("d")? == keycode {
-            //debug!("dmenu_run");
-            spawn_process("dmenu_recency", vec![]);
-            return Some(());
-        }
-        if ws_keys.contains(&keycode) {
-            //debug!("mod_not_shift switch ws");
-            let ws_num = keycode_to_ws(keycode);
-            wm::set_current_ws(state, ws_num)?;
+        match into_hdl_keysym(&state.lib.keycode_to_key_sym(keycode)) {
+            HDLKeysym::XK_f => {
+                let ww = state
+                    .monitors
+                    .get_mut(&state.current_monitor)?
+                    .remove_window(state.focus_w)?;
+                let new_ww = wm::toggle_maximize(state, ww);
+                state
+                    .monitors
+                    .get_mut(&state.current_monitor)?
+                    .add_window(state.focus_w, new_ww);
+            }
+
+            HDLKeysym::XK_Right | HDLKeysym::XK_l => {
+                shift_window(state, Direction::East);
+            }
+
+            HDLKeysym::XK_Left | HDLKeysym::XK_h => {
+                shift_window(state, Direction::West);
+            }
+            HDLKeysym::XK_Down | HDLKeysym::XK_j => {
+                shift_window(state, Direction::South);
+            }
+            HDLKeysym::XK_Up | HDLKeysym::XK_k => {
+                shift_window(state, Direction::North);
+            }
+            HDLKeysym::XK_c => {
+                let mon = state.monitors.get_mut(&state.current_monitor)?;
+                let ww = mon.remove_window(state.focus_w)?;
+                let (pos, size) = mon.place_window(ww.window());
+                let new_ww = WindowWrapper {
+                    window_rect: Rect::new(size, pos),
+                    previous_state: ww.current_state,
+                    current_state: WindowState::Free,
+                    handle_state: HandleState::Center.into(),
+                    ..ww
+                };
+                mon.add_window(state.focus_w, new_ww);
+            }
+            HDLKeysym::XK_d => {
+                spawn_process("dmenu_recency", vec![]);
+            }
+            _ => {
+                if ws_keys.contains(&keycode) {
+                    //debug!("mod_not_shift switch ws");
+                    let ws_num = keycode_to_ws(keycode);
+                    wm::set_current_ws(state, ws_num)?;
+                }
+            }
         }
     }
     Some(())
