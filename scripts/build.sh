753ae50539daeda359096cb6c7f013ef7598cf11
PierreLeidbring <pierre.leidbring@gmail.com>
2020-02-06 14:45:55 +0100

    small update to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,18 +3,30 @@
 NAME=hadlock
 DIR=/usr/local/bin/
 
-if [ -f "$DIR$NAME" ]; then
-	echo "deleting: $DIR$NAME"
-	sudo rm $DIR$NAME
+function delete_old_bin () {
+	if [ -f "$DIR$NAME" ]; then
+		echo "deleting: $DIR$NAME"
+		sudo rm $DIR$NAME
+	fi
+}
+
+
+if [ "$1" == "debug" ]; then
+	delete_old_bin
+	echo "Building debug binary"
+	cargo build
+	sudo cp ../target/debug/hadlock $DIR
+elif [ "$1" == "release" ]; then
+	delete_old_bin
+	echo "Building release binary"
+	cargo build --release
+	sudo cp ../target/release/hadlock $DIR
+else
+	echo "Please provide argument \"debug\" or \"release\""
+	exit 1
 fi
 
-cargo build
-
-sudo cp ../target/debug/hadlock $DIR
-
 LOG=~/hadlock.log
-
-
 if [ -f "$LOG" ]; then
 	echo "deleting: $LOG"
 	sudo rm $LOG
