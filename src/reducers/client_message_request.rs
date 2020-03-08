3c71c7d5f03818567bff4842c200477aea57979c
PierreLeidbring <pierre.leidbring@gmail.com>
2020-03-08 12:50:58 +0100

    small refactor of removing and adding windows

--- src/reducers/client_message_request.rs
+++ src/reducers/client_message_request.rs
@@ -78,12 +78,7 @@ impl Reducer<action::ClientMessageRequest> for State {
                 .monitors
                 .get_mut(&self.current_monitor)
                 .expect("ClientMessageRequest - monitor - get_mut");
-            let old_ww = mon.remove_window(action.win).unwrap();
-            let new_ww = wm::toggle_monocle(mon, old_ww);
-            self.monitors
-                .get_mut(&self.current_monitor)
-                .expect("ClientMessageRequest - monitor - get_mut")
-                .add_window(action.win, new_ww);
+            mon.swap_window(action.win, |mon, ww| wm::toggle_monocle(mon, ww));
         }
     }
 }
