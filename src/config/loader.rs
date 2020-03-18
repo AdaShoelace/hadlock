f8a0de6393d484b2cb013ea202f40979b443f41e
PierreLeidbring <pierre.leidbring@gmail.com>
2020-03-18 22:14:51 +0100

    error handling in config/loader.rs

--- src/config/loader.rs
+++ src/config/loader.rs
@@ -29,8 +29,13 @@ pub(super) fn load_config() -> Config {
         let mut file_content = String::new();
         file.read_to_string(&mut file_content)
             .expect(&format!("Failed to read file content: {:?}", path));
-        let config: Config = serde_json::from_str(&file_content).expect("Failed to map config");
-        Config::from(config)
+        match serde_json::from_str(&file_content) {
+            Ok(config) => Config::from(config),
+            Err(err) => {
+                error!("{}", err);
+                Config::default()
+            }
+        }
     } else {
         debug!("Path either doesn't exist or is not a file");
         Config::default()
