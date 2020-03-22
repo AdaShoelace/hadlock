a6ff9cc3b864c714a83f97ed929a9f76683a8f1f
PierreLeidbring <pierre.leidbring@gmail.com>
2020-03-22 20:38:32 +0100

    fix focus of maximized window w/ sending to new ws

--- src/reducers/property_notify.rs
+++ src/reducers/property_notify.rs
@@ -18,6 +18,6 @@ use {
 impl Reducer<action::PropertyNotify> for State {
     fn reduce(&mut self, action: action::PropertyNotify) {
         let name = self.lib.atom_name(action.atom).unwrap();
-        debug!("PropertyNotify, Atom name: {}", name);
+        //debug!("PropertyNotify, Atom name: {}", name);
     }
 }
