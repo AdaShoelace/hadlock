49f8e131b5caedc6ee69fda1d0a2521fa8f6a0a7
PierreLeidbring <pierre.leidbring@gmail.com>
2020-04-04 19:29:14 +0200

    refactor map_request

--- src/reducers/map_request.rs
+++ src/reducers/map_request.rs
@@ -22,96 +22,19 @@ use {
 
 impl Reducer<action::MapRequest> for State {
     fn reduce(&mut self, action: action::MapRequest) {
-        match self.lib.transient_for_hint(action.win) {
-            Some(_win) => {
-                debug!(
-                    "Window type: {} is transient",
-                    self.lib.get_window_type(action.win).get_name()
-                );
-                let trans_size = {
-                    let attr = self.lib.get_window_attributes(action.win);
-                    Size {
-                        width: attr.width,
-                        height: attr.height,
-                    }
-                };
-                let mon = self
-                    .monitors
-                    .get_mut(&self.current_monitor)
-                    .expect("Shit went down in map_request");
+        if let Some(_win) = self.lib.transient_for_hint(action.win) {
+            handle_transient_window(self, &action);
+            return
+        }
 
-                let pos = match mon.get_client(action.parent) {
-                    Some(win) => {
-                        let (pos, size) = (win.get_position(), win.get_size());
-                        Position {
-                            x: pos.x + (size.width / 2) - (trans_size.width / 2) as i32,
-                            y: pos.y + (size.height / 2) - (trans_size.height / 2) as i32,
-                        }
-                    }
-                    None if action.parent == self.lib.get_root() => {
-                        let screen = mon.screen.clone();
-                        let (pos, size) = (
-                            Position {
-                                x: screen.x,
-                                y: screen.y,
-                            },
-                            Size {
-                                width: screen.width as i32,
-                                height: screen.height as i32,
-                            },
-                        );
-                        Position {
-                            x: pos.x + (size.width / 2) - (trans_size.width / 2) as i32,
-                            y: pos.y + (size.height / 2) - (trans_size.height / 2) as i32,
-                        }
-                    }
-                    None => return,
-                };
-                let ww = WindowWrapper::new(
-                    action.win,
-                    Rect::new(
-                        pos,
-                        Size {
-                            width: trans_size.width as i32,
-                            height: trans_size.height as i32,
-                        },
-                    ),
-                    true,
-                );
-                mon.add_window(
-                    ww.window(),
-                    WindowWrapper {
-                        handle_state: HandleState::New.into(),
-                        ..ww
-                    },
-                );
-                return;
-            }
-            None => (),
-        };
         debug!(
             "MapRequest - window: {} - Parent: {}",
             action.win, action.parent
         );
+
         if self.lib.get_window_type(action.win) == WindowType::Dock {
-            match self.lib.get_window_strut_array(action.win) {
-                Some(dock) => {
-                    debug!("Mapping window is dock!");
-                    let w_geom = self.lib.get_geometry(action.win);
-                    let mon = self
-                        .monitors
-                        .values_mut()
-                        .filter(|mon| wm::window_inside_screen(&w_geom, &mon.screen))
-                        .collect::<Vec<&mut Monitor>>()
-                        .remove(0);
-                    mon.set_dock_area(dock);
-                    self.lib
-                        .select_input(action.win, PointerMotionMask | SubstructureRedirectMask);
-                    self.lib.map_window(action.win);
-                    return;
-                }
-                None => return,
-            }
+            handle_dock(self, &action);
+            return
         }
 
         if !self.lib.should_be_managed(action.win) {
@@ -151,19 +74,36 @@ impl Reducer<action::MapRequest> for State {
             debug!(
                 "Windows in mon before place_window: {:?}",
                 mon.get_current_ws()
-                    .unwrap()
-                    .clients
-                    .keys()
-                    .collect::<Vec<&Window>>()
+                .unwrap()
+                .clients
+                .keys()
+                .collect::<Vec<&Window>>()
             );
             let window_amount = windows.len();
-            let _ = windows.into_iter().for_each(|(win, rect)| {
+            //let _ = windows.into_iter().for_each(|(win, rect)| {
+            for (win, rect) in windows.into_iter() {
                 match mon.remove_window(win) {
                     Some(ww) => {
+                        let handle_state = if ww.current_state == WindowState::Maximized ||
+                            ww.current_state == WindowState::Monocle {
+                                vec![
+                                    HandleState::MaximizeRestore,
+                                    HandleState::Move,
+                                    HandleState::Resize,
+                                    HandleState::Unfocus,
+                                ]
+                            } else {
+                                vec![
+                                    HandleState::Move,
+                                    HandleState::Resize,
+                                    HandleState::Unfocus,
+                                ]
+
+                            };
                         let ww = WindowWrapper {
                             window_rect: rect,
-                            current_state: WindowState::Free,
-                            handle_state: vec![HandleState::Move, HandleState::Resize].into(),
+                            current_state: WindowState::Tiled,
+                            handle_state: handle_state.into(),
                             ..ww
                         };
                         mon.add_window(win, ww);
@@ -180,22 +120,103 @@ impl Reducer<action::MapRequest> for State {
                                     .replace(vec![HandleState::New, HandleState::Maximize]);
                                 ww
                             } else {
-                                WindowWrapper::new(action.win, rect, false)
+                                let mut ww = WindowWrapper::new(action.win, rect, false);
+                                ww.append_handle_state(vec!{HandleState::Focus});
+                                self.focus_w = action.win;
+                                ww
                             };
                             ww
                         };
                         mon.add_window(action.win, ww);
                     }
                 };
-            });
+            } //);
             debug!(
                 "Windows in mon after place_window: {:?}",
                 mon.get_current_ws()
-                    .unwrap()
-                    .clients
-                    .keys()
-                    .collect::<Vec<&Window>>()
+                .unwrap()
+                .clients
+                .keys()
+                .collect::<Vec<&Window>>()
             );
         }
+        }
     }
-}
+
+    fn handle_transient_window(state: &mut State, action: &action::MapRequest) {
+        debug!(
+            "Window type: {} is transient",
+            state.lib.get_window_type(action.win).get_name()
+        );
+        let trans_size = {
+            let attr = state.lib.get_window_attributes(action.win);
+            Size {
+                width: attr.width,
+                height: attr.height,
+            }
+        };
+        let mon = state
+            .monitors
+            .get_mut(&state.current_monitor)
+            .expect("Shit went down in map_request");
+
+        let pos = match mon.get_client(action.parent) {
+            Some(win) => {
+                let (pos, size) = (win.get_position(), win.get_size());
+                Position {
+                    x: pos.x + (size.width / 2) - (trans_size.width / 2) as i32,
+                    y: pos.y + (size.height / 2) - (trans_size.height / 2) as i32,
+                }
+            }
+            None if action.parent == state.lib.get_root() => {
+                let screen = mon.screen.clone();
+                let (pos, size) = screen.into();
+                Position {
+                    x: pos.x + (size.width / 2) - (trans_size.width / 2) as i32,
+                    y: pos.y + (size.height / 2) - (trans_size.height / 2) as i32,
+                }
+            }
+            None => return,
+        };
+        let ww = WindowWrapper::new(
+            action.win,
+            Rect::new(
+                pos,
+                Size {
+                    width: trans_size.width as i32,
+                    height: trans_size.height as i32,
+                },
+            ),
+            true,
+        );
+        mon.add_window(
+            ww.window(),
+            WindowWrapper {
+                handle_state: HandleState::New.into(),
+                ..ww
+            },
+        );
+        return;
+    }
+
+    fn handle_dock(state: &mut State, action: &action::MapRequest) {
+
+        match state.lib.get_window_strut_array(action.win) {
+            Some(dock) => {
+                debug!("Mapping window is dock!");
+                let w_geom = state.lib.get_geometry(action.win);
+                let mon = state
+                    .monitors
+                    .values_mut()
+                    .filter(|mon| wm::window_inside_screen(&w_geom, &mon.screen))
+                    .collect::<Vec<&mut Monitor>>()
+                    .remove(0);
+                mon.set_dock_area(dock);
+                state.lib
+                    .select_input(action.win, PointerMotionMask | SubstructureRedirectMask);
+                state.lib.map_window(action.win);
+                return;
+            }
+            None => return,
+        }
+    }
