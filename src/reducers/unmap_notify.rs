f4ce10c4439980640f5fa400ac0bf1085a11d8f5
PierreLeidbring <pierre.leidbring@gmail.com>
2020-07-19 12:10:26 +0200

    fix warnings & errors

--- src/reducers/unmap_notify.rs
+++ src/reducers/unmap_notify.rs
@@ -1,23 +1,14 @@
-use {
-    crate::{
-        config::CONFIG,
-        models::{rect::*, window_type::WindowType, windowwrapper::*, internal_action::InternalAction},
-        state::State,
-        wm,
-        xlibwrapper::action,
-        xlibwrapper::core::*,
-        xlibwrapper::util::*,
-        xlibwrapper::xlibmodels::*,
-    },
-    reducer::*,
-    std::rc::Rc,
-};
+use crate::{models::internal_action::InternalAction, state::State, wm, xlibwrapper::action};
+use reducer::*;
 
 impl Reducer<action::UnmapNotify> for State {
     fn reduce(&mut self, action: action::UnmapNotify) {
         //debug!("UnmapNotify");
         let mon_id = wm::get_mon_by_window(&self, action.win).unwrap_or(self.current_monitor);
-        let mon = self.monitors.get_mut(&mon_id).expect("No monitor was found?!");
+        let mon = self
+            .monitors
+            .get_mut(&mon_id)
+            .expect("No monitor was found?!");
         mon.remove_window(action.win);
         let _ = self.tx.send(InternalAction::UpdateLayout);
     }
