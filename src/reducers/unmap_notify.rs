6a25d1b5af5efb7efc19c00e82dee72010095dca
PierreLeidbring <pierre.leidbring@gmail.com>
2020-07-19 12:08:55 +0200

    fix hidden window behaviour

--- src/reducers/unmap_notify.rs
+++ src/reducers/unmap_notify.rs
@@ -1,22 +1,24 @@
-#![allow(unused_imports)]
 use {
     crate::{
         config::CONFIG,
-        models::{rect::*, window_type::WindowType, windowwrapper::*},
+        models::{rect::*, window_type::WindowType, windowwrapper::*, internal_action::InternalAction},
         state::State,
+        wm,
         xlibwrapper::action,
         xlibwrapper::core::*,
         xlibwrapper::util::*,
         xlibwrapper::xlibmodels::*,
     },
     reducer::*,
-    std::cell::RefCell,
     std::rc::Rc,
 };
 
 impl Reducer<action::UnmapNotify> for State {
-    fn reduce(&mut self, _action: action::UnmapNotify) {
+    fn reduce(&mut self, action: action::UnmapNotify) {
         //debug!("UnmapNotify");
-        //self.lib.unmap_window(action.win);
+        let mon_id = wm::get_mon_by_window(&self, action.win).unwrap_or(self.current_monitor);
+        let mon = self.monitors.get_mut(&mon_id).expect("No monitor was found?!");
+        mon.remove_window(action.win);
+        let _ = self.tx.send(InternalAction::UpdateLayout);
     }
 }
