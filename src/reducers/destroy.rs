0779d6239e712094c468af004a7b980829de2ae9
PierreLeidbring <pierre.leidbring@gmail.com>
2020-07-21 19:12:25 +0200

    fix focus issue on destroying window

--- src/reducers/destroy.rs
+++ src/reducers/destroy.rs
@@ -1,10 +1,5 @@
 use {
-    crate::{
-        layout::LayoutTag,
-        state::State,
-        wm,
-        xlibwrapper::action,
-    },
+    crate::{layout::LayoutTag, state::State, wm, xlibwrapper::action},
     reducer::*,
 };
 
@@ -20,7 +15,15 @@ impl Reducer<action::Destroy> for State {
             .get_mut(&self.current_monitor)
             .expect("DestroyNotify - get_mut");
         if mon.contains_window(action.win) {
+            let next_focus = mon
+                .get_previous(action.win)
+                .map(|ww| ww.window())
+                .unwrap_or(self.lib.get_root());
             mon.remove_window(action.win);
+            if let Some(ws) = mon.get_current_ws_mut() {
+                ws.focus_w = next_focus;
+            }
+            self.focus_w = next_focus;
             if mon.get_current_layout() != LayoutTag::Floating {
                 wm::reorder(self);
             }
