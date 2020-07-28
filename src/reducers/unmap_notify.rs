d25f3eac3dc6eb1d3397ecdd5352817cfbfd9cab
PierreLeidbring <pierre.leidbring@gmail.com>
2020-07-28 22:43:46 +0200

    focus on unmap

--- src/reducers/unmap_notify.rs
+++ src/reducers/unmap_notify.rs
@@ -10,7 +10,24 @@ impl Reducer<action::UnmapNotify> for State {
             .get_mut(&mon_id)
             .expect("No monitor was found?!");
         if let Some(client) = mon.get_client(action.win) {
+            debug!("unmap - contains action.win: 0x{:x}", action.win);
             if client.current_state != WindowState::Destroy {
+                let next_focus = mon
+                    .get_previous(action.win)
+                    .map(|ww| ww.window())
+                    .unwrap_or(
+                        mon.get_newest()
+                            .map(|(win, _)| *win)
+                            .unwrap_or(self.lib.get_root()),
+                    );
+                debug!(
+                    "in destroy - action.win: {}, next_focus: {}",
+                    action.win, next_focus
+                );
+                if let Some(ws) = mon.get_current_ws_mut() {
+                    ws.focus_w = next_focus;
+                }
+                self.focus_w = next_focus;
                 mon.remove_window(action.win);
             }
         }
